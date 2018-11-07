c308a7ecc983a95a31857a50ee81fcfc9df16fd8
pingtianxia <henanyunpingearl@163.com>
2018-11-07 11:28:22 +0800

    Hystrix 监控指标 Dashboard

--- 201811eureka-microservice-order-hystrix/src/main/java/com/microservice/eureka/microserviceorderhystrix/MicroServiceOrderHystrixApplication.java
+++ 201811eureka-microservice-order-hystrix/src/main/java/com/microservice/eureka/microserviceorderhystrix/MicroServiceOrderHystrixApplication.java
@@ -5,10 +5,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Hystrix 插件监控状态的路径
+ * http://192.168.1.138:9973/health
+ *
+ * Hystrix Metrics Stream:
+ *
+ * To enable the Hystrix metrics stream include a dependency on spring-boot-starter-actuator.
+ * This will expose the /hystrix.stream as a management endpoint.
+ * <dependency>
+ *    <groupId>org.springframework.boot</groupId>
+ *    <artifactId>spring-boot-starter-actuator</artifactId>
+ * </dependency>
+ * http://192.168.1.138:9973/hystrix.stream
+ *
+ * Hystrix Dashboard:
+ * To include the Hystrix Dashboard in your project use the starter with group org.springframework.cloud and artifact id spring-cloud-starter-netflix-hystrix-dashboard
+ * http://192.168.1.138:9973/hystrix
+ */
+
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MicroServiceOrderHystrixApplication {
 
     public static void main(String[] args) {
